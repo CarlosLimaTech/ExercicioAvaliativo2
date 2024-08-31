9509d6ac428a51ae39ef44ff9bad8d25e3f3a22d
carlos_lima <vieira.eduardo@aluno.ifsp.edu.br>
2024-08-31 17:34:38 -0300

    Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,35 +1,34 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
 }
 
 android {
-    namespace = "br.edu.ifsp.dmo.diario"
+    namespace = "br.erossi.tasksroom"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "br.edu.ifsp.dmo.diario"
+        applicationId = "br.erossi.tasksroom"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -40,12 +39,17 @@ android {
 }
 
 dependencies {
+    val version = "2.6.1"
+    implementation("androidx.room:room-runtime:$version")
+    implementation("androidx.room:room-ktx:$version")
+    kapt("androidx.room:room-compiler:$version")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
