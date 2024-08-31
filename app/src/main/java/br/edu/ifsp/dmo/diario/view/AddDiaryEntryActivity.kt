0f8a4939011d1d80b5ce3f09416fb213914115ad
carlos_lima <vieira.eduardo@aluno.ifsp.edu.br>
2024-08-31 19:09:56 -0300

    Ajuste no item

--- app/src/main/java/br/edu/ifsp/dmo/diario/view/AddDiaryEntryActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/diario/view/AddDiaryEntryActivity.kt
@@ -44,10 +44,19 @@ class AddDiaryEntryActivity : AppCompatActivity() {
             binding.editTextContent.setText(it.content)
             binding.editTextLocation.setText(it.location)
             selectedDate = it.date
-            binding.buttonSelectDate.text = it.date.toString()  // Formate a data conforme necessário
-            // Se precisar, formate a hora também
+
+            // Formatar e exibir apenas a data
+            val calendar = Calendar.getInstance()
+            calendar.time = it.date
+            val date = "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.YEAR)}"
+            binding.buttonSelectDate.text = date
+
+            // Formatar e exibir apenas a hora
+            val time = "${calendar.get(Calendar.HOUR_OF_DAY)}:${calendar.get(Calendar.MINUTE)}"
+            binding.buttonSelectTime.text = time
         }
     }
+
 
     private fun setupDateAndTimePickers() {
         binding.buttonSelectDate.setOnClickListener {
