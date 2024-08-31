357e3aaf31203f98f6d4eee1d111ea154cb350d2
carlos_lima <vieira.eduardo@aluno.ifsp.edu.br>
2024-08-31 17:39:50 -0300

    DiaryEntry

---
+++ app/src/main/java/br/edu/ifsp/dmo/diario/model/DiaryEntry.kt
@@ -0,0 +1,15 @@
+package br.edu.ifsp.dmo.diario.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "diary_entries")
+data class DiaryEntry(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val content: String,
+    val location: String,
+    val date: Date
+)
